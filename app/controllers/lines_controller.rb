7bacd0942ddde18e01c531468ed81a06586d94bb
Lesley Park <lesparkley@gmail.com>
2016-10-26 05:48:40 +0000

    Added simple form gem + modal

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,4 +2,23 @@ class LinesController < ApplicationController
   def index
     @lines = Line.all
   end
+
+  def new
+    @line = Line.new
+  end
+
+  def create
+    Line.create(line_params)
+    if @line.invalid?
+      flash[:error] = '<strong>Could not save</strong> please make sure all fields are filled out.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def line_params
+    params.require(:line).permit(:message, :name)
+  end
+
 end
--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,4 @@
 class Line < ActiveRecord::Base
+  validates :message, presence: true
+  validates :name, presence: true
 end
